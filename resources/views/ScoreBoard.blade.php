1e3555c929996593e6117ade4d2f54472c53288a
bakkar <bokkor3256@gmail.com>
2020-10-21 19:40:28 +0600

    finish project
    finish project

--- resources/views/ScoreBoard.blade.php
+++ resources/views/ScoreBoard.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-4 p-3">
             <div class="card text-center">
                 <div class="card-body">
-                    <h2>45656</h2>
+                    <h2 id="score">......</h2>
                     <h5>Live Score</h5>
                 </div>
             </div>
@@ -16,4 +16,13 @@
 @endsection
 
 @section('script')
+<script type="text/javascript">
+    var pusher = new Pusher('ccfe7f924dbb9c6f0263',{
+        cluster:'ap2'
+    })
+    var channel = pusher.subscribe('score-channel');
+    channel.bind('score-event',function (data) {
+        $('#score').html(JSON.stringify(data));
+    })
+</script>
 @endsection
